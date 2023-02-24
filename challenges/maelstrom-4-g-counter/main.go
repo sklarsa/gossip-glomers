6edd7d9121a0ce8630cbddbc29bb273933b95d35
Steve Sklar <sklarsa@gmail.com>
2023-02-23 19:14:59 -0500

    ugh trying gossip

--- challenges/maelstrom-4-g-counter/main.go
+++ challenges/maelstrom-4-g-counter/main.go
@@ -12,70 +12,99 @@ import (
 )
 
 var (
-	writeKey    = "writing"
-	readKey     = "reading"
-	registerKey = "register"
-	ctx         = context.TODO()
+	ctx            = context.TODO()
+	gossipInterval = 1 * time.Second
 )
 
-func write(kv *maelstrom.KV, val int) error {
-	var (
-		rpcError *maelstrom.RPCError
-		err      error
-	)
-
-	err = kv.Write(ctx, writeKey, true)
-	if err != nil {
-		return err
+func getVisibleNodeIds(n *maelstrom.Node, topology map[string]any) []string {
+	retVal := []string{}
+	nodes, ok := topology[n.ID()]
+	if !ok {
+		return retVal
 	}
 
-	count, err := kv.ReadInt(ctx, registerKey)
-	if err != nil {
-		if errors.As(err, &rpcError) && rpcError.Code == maelstrom.KeyDoesNotExist {
-			count = 0
-		} else {
-			return err
-		}
+	for _, n := range nodes.([]any) {
+		retVal = append(retVal, n.(string))
 	}
+	return retVal
 
-	err = kv.Write(ctx, registerKey, int(val)+count)
-	if err != nil {
-		return err
-	}
-
-	return kv.Write(ctx, writeKey, false)
 }
 
-func read(kv *maelstrom.KV) (int, error) {
-	var err error
-	err = kv.Write(ctx, readKey, time.Now())
-	if err != nil {
-		return 0, err
-	}
-
-	val, err := kv.ReadInt(ctx, registerKey)
-
-	if err != nil {
-		var rpcError *maelstrom.RPCError
-		if errors.As(err, &rpcError) {
-			if rpcError.Code == maelstrom.KeyDoesNotExist {
-				return 0, nil
-			}
-		}
+func readCount(thisNodeId string, kv *maelstrom.KV, targetNodeId string) (int, error) {
+	val, err := kv.ReadInt(ctx, fmt.Sprintf("%s-%s", thisNodeId, targetNodeId))
+	var rpcErr *maelstrom.RPCError
+	if errors.As(err, &rpcErr) && rpcErr.Code == maelstrom.KeyDoesNotExist {
+		val = 0
+		err = nil
 	}
 	return val, err
+}
+
+func writeCount(n *maelstrom.Node, kv *maelstrom.KV, nodeId string, count int) error {
+	return kv.Write(ctx, fmt.Sprintf("%s-%s", n.ID(), nodeId), count)
 }
 
 func main() {
 	n := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(n)
+	ticker := time.NewTicker(gossipInterval)
+	topology := map[string]any{}
+
+	go func() {
+		// Gossip here
+		if topology == nil {
+			return
+		}
+
+		visibleNodes := getVisibleNodeIds(n, topology)
+		for _, id := range visibleNodes {
+			count, err := readCount(n.ID(), kv, id)
+			if err != nil {
+				panic(err)
+			}
+			body := map[string]any{
+				"type":  "gossip",
+				"count": count,
+			}
+
+			err = n.Send(id, body)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+	}()
+
+	n.Handle("gossip", func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		nodeId := msg.Src
+		gossipCount := int(body["count"].(float64))
+
+		storeCount, err := readCount(n.ID(), kv, nodeId)
+		if err != nil {
+			return err
+		}
+
+		if gossipCount > storeCount {
+			err = writeCount(n, kv, nodeId, gossipCount)
+			if err != nil {
+				return err
+			}
+
+		}
+
+		return nil
+
+	})
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 
-		var (
-			body map[string]any
-			err  error
-		)
+		var body map[string]any
 
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
@@ -85,32 +114,57 @@ func main() {
 		if !ok {
 			return fmt.Errorf("cannot type assert 'delta' to float64: %v", body)
 		}
-		err = write(kv, int(val))
-		for err != nil {
-			err = write(kv, int(val))
+
+		count, err := readCount(n.ID(), kv, n.ID())
+		if err != nil {
+			return err
 		}
 
-		body["type"] = "add_ok"
-		delete(body, "delta")
+		err = writeCount(n, kv, n.ID(), count+int(val))
+		if err != nil {
+			return err
+		}
 
-		return n.Reply(msg, body)
+		return n.Reply(msg, map[string]any{
+			"type": "add_ok",
+		})
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		val, err := read(kv)
-		for err != nil {
-			val, err = read(kv)
+		sum := 0
+		for _, id := range n.NodeIDs() {
+			for _, id2 := range n.NodeIDs() {
+				count, err := readCount(id, kv, id2)
+				if err != nil {
+					return err
+				}
+				sum += count
+			}
+
 		}
 
-		body := map[string]any{
+		return n.Reply(msg, map[string]any{
 			"type":  "read_ok",
-			"value": val,
-		}
-		return n.Reply(msg, body)
+			"value": sum,
+		})
+	})
 
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology = body["topology"].(map[string]any)
+
+		return n.Reply(msg, map[string]any{
+			"type": "topology_ok",
+		})
 	})
 
 	if err := n.Run(); err != nil {
+		ticker.Stop()
 		log.Fatal(err)
 	}
 
